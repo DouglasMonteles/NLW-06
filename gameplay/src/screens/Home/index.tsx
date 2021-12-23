71772b65d64b5a34ad9445ba83a12a697f9a0996
Douglas Monteles <silvadouglas328@gmail.com>
2021-12-23 16:30:14 -0300

    Criando componente de Profile do usuário

--- gameplay/src/screens/Home/index.tsx
+++ gameplay/src/screens/Home/index.tsx
@@ -2,11 +2,13 @@ import { View } from "react-native";
 
 import { styles } from './styles';
 
+import { Profile } from "../../components/Profile";
+
 export function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-
+        <Profile />
       </View>
     </View>
   );
