d2a1b495a73c69a53774ee5401610714f5ee3dbc
Douglas Monteles <silvadouglas328@gmail.com>
2021-12-23 16:46:28 -0300

    Criando componente de Avatar

--- gameplay/src/components/Profile/index.tsx
+++ gameplay/src/components/Profile/index.tsx
@@ -1,10 +1,15 @@
 import { Text, View } from "react-native";
+import { Avatar } from "../Avatar";
 
 import { styles } from './styles';
 
 export function Profile() {
   return (
     <View style={styles.container}>
+      <Avatar 
+        urlImage="https://github.com/douglasmonteles.png"
+      />
+
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>
