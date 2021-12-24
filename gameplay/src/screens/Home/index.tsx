1bd4d2b92933b8ec39bc9b2acfab8082c2efecb5
Douglas Monteles <silvadouglas328@gmail.com>
2021-12-23 23:49:43 -0300

    Criando componente de botão de adição

--- gameplay/src/screens/Home/index.tsx
+++ gameplay/src/screens/Home/index.tsx
@@ -3,12 +3,16 @@ import { View } from "react-native";
 import { styles } from './styles';
 
 import { Profile } from "../../components/Profile";
+import { ButtonAdd } from "../../components/ButtonAdd";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Profile />
+        <ButtonAdd 
+          activeOpacity={0.7}
+        />
       </View>
     </View>
   );
