3b4e0d92673c88f1304cead86d4711f178183abc
Douglas Monteles <silvadouglas328@gmail.com>
2021-12-24 14:00:42 -0300

    Exibindo itens na FlatList e adicionando componente de separação de itens

---
+++ gameplay/src/components/ListDivider/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+
+import { styles } from "./styles";
+
+export function ListDivider() {
+
+  return (
+    <View 
+      style={styles.container}
+    />
+  );
+}
--- gameplay/src/screens/Home/index.tsx
+++ gameplay/src/screens/Home/index.tsx
@@ -11,12 +11,52 @@ import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
 import { ListHeader } from "../../components/ListHeader";
 import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
 
 export function Home() {
   const [category, setCategory] = useState('');
   const appointments = [
     {
       id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder sem perder uma partida da md10',
+    },
+
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder sem perder uma partida da md10',
+    },
+
+    {
+      id: '3',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder sem perder uma partida da md10',
+    },
+
+    {
+      id: '4',
       guild: {
         id: '1',
         name: 'Lendários',
@@ -40,28 +80,28 @@ export function Home() {
         <ButtonAdd />
       </View>
 
-      <View>
-        <CategorySelect 
-          categorySelected={category}
-          setCategory={handleCategorySelected}
+      <CategorySelect 
+        categorySelected={category}
+        setCategory={handleCategorySelected}
+      />
+
+      <View style={styles.content}>
+        <ListHeader 
+          title="Partidas agendadas"
+          subtitle="Total 6"
         />
 
-        <View style={styles.content}>
-          <ListHeader 
-            title="Partidas agendadas"
-            subtitle="Total 6"
-          />
-
-          <FlatList 
-            data={appointments}
-            keyExtractor={item => item.id}
-            renderItem={({item}) => (
-              <Appointment 
-                data={item}
-              />
-            )}
-          />
-        </View>
+        <FlatList 
+          data={appointments}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <Appointment 
+              data={item}
+            />
+          )}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+        />
       </View>
     </View>
   );
