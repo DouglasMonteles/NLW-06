665158f47e5a9683b89d3197e49676e70bc1429c
Douglas Monteles <silvadouglas328@gmail.com>
2021-12-24 12:16:10 -0300

    Criando a listagem e seleção de categoria

--- gameplay/src/screens/Home/index.tsx
+++ gameplay/src/screens/Home/index.tsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import { View } from "react-native";
 
 import { styles } from './styles';
 
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
 
 export function Home() {
+  const [category, setCategory] = useState('');
+
+  function handleCategorySelected(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Profile />
-        <ButtonAdd 
-          activeOpacity={0.7}
+        <ButtonAdd />
+      </View>
+
+      <View>
+        <CategorySelect 
+          categorySelected={category}
+          setCategory={handleCategorySelected}
         />
       </View>
     </View>
