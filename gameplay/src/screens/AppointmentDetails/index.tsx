e4d1e3bbc1e36b72bed720ee7215a5310087f780
Douglas Monteles <silvadouglas328@gmail.com>
2021-12-25 20:37:57 -0300

    Exibindo dados dos jogadores

--- gameplay/src/screens/AppointmentDetails/index.tsx
+++ gameplay/src/screens/AppointmentDetails/index.tsx
@@ -1,5 +1,5 @@
 import { Fontisto } from "@expo/vector-icons";
-import { ImageBackground, Text, View } from "react-native";
+import { FlatList, ImageBackground, Text, View } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
 
 import { styles } from "./styles";
@@ -9,8 +9,26 @@ import BannerImg from '../../assets/banner.png';
 
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
+import { ListHeader } from "../../components/ListHeader";
+import { Member } from "../../components/Member";
+import { ListDivider } from "../../components/ListDivider";
 
 export function AppointmentDetails() {
+  const members = [
+    {
+      id: '1',
+      username: 'Douglas',
+      avatar_url: 'https://github.com/douglasmonteles.png',
+      status: 'online',
+    },
+
+    {
+      id: '2',
+      username: 'Silva',
+      avatar_url: 'https://github.com/douglasmonteles.png',
+      status: 'offline',
+    },
+  ];
 
   return (
     <Background>
@@ -43,6 +61,24 @@ export function AppointmentDetails() {
           </Text>
         </View>
       </ImageBackground>
+
+      <ListHeader 
+        title="Jogadores"
+        subtitle="Total 3"
+      />
+
+      <FlatList 
+        data={members}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <Member 
+            key={item.id}
+            data={item}
+          />
+        )}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.members}
+      />
     </Background>
   );
 }
